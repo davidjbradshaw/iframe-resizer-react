6cdd1cf5daa014e05741d3a8015b945c4b01c331
David J. Bradshaw <dave@bradshaw.net>
2019-09-16 16:12:29 +0100

    Add rollup config to eslint

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
+import svgr from '@svgr/rollup'
 import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
+import resolve from 'rollup-plugin-node-resolve'
 import external from 'rollup-plugin-peer-deps-external'
 import postcss from 'rollup-plugin-postcss'
-import resolve from 'rollup-plugin-node-resolve'
 import url from 'rollup-plugin-url'
-import svgr from '@svgr/rollup'
 
 import pkg from './package.json'
 
