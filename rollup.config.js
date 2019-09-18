9ca39d2861cc093bd10bcf616d8474aac6ac0265
David J. Bradshaw <dave@bradshaw.net>
2019-09-18 21:27:55 +0100

    Add rollup-plugin-copy

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import svgr from '@svgr/rollup'
 import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
+import copy from 'rollup-plugin-copy'
 import resolve from 'rollup-plugin-node-resolve'
 import external from 'rollup-plugin-peer-deps-external'
 import postcss from 'rollup-plugin-postcss'
@@ -34,5 +35,18 @@ export default {
     }),
     resolve(),
     commonjs(),
+    copy({
+      targets: [
+        {
+          src: '../iframe-resizer/example/frame.*',
+          dest: 'example/public/html',
+        },
+        {
+          src:
+            'node_modules/iframe-resizer/js/iframeResizer.contentWindow.min.js',
+          dest: 'example/public/js',
+        },
+      ],
+    }),
   ],
 }
