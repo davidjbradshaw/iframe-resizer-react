11bf4f99d61dcc766eaa0977982bcb6fdc7a85a1
David J. Bradshaw <dave@bradshaw.net>
2024-04-24 13:07:49 +0100

    Update BrowserList and paths

--- example/src/app.jsx
+++ example/src/app.jsx
@@ -1,34 +1,34 @@
-import IframeResizer from 'iframe-resizer-react'
-import React, { useRef, useState } from 'react'
+import IframeResizer from "../../";
+import React, { useRef, useState } from "react";
 
-import MessageData from './message-data'
+import MessageData from "./message-data";
 
 export default () => {
-  const ref = useRef(null)
-  const [messageData, setMessageData] = useState(undefined)
+  const ref = useRef(null);
+  const [messageData, setMessageData] = useState(undefined);
 
-  const onResized = (data) => setMessageData(data)
+  const onResized = (data) => setMessageData(data);
 
   const onMessage = (data) => {
-    setMessageData(data)
-    ref.current.sendMessage('Hello back from parent page')
-  }
+    setMessageData(data);
+    ref.current.sendMessage("Hello back from parent page");
+  };
 
   return (
-    <div style={{ margin: '20px' }}>
+    <div style={{ margin: "20px" }}>
       <h2>Automagically resizing React iFrame component</h2>
       <p>
         Resize window or click one of the links in the iFrame to watch it
         resize.
       </p>
-      <div style={{ margin: '20px 0' }}>
+      <div style={{ margin: "20px 0" }}>
         <IframeResizer
           log
           inPageLinks
           forwardRef={ref}
           onMessage={onMessage}
           onResized={onResized}
-          src="html/frame.content.html"
+          src="/iframe-resizer-react/html/frame.content.html"
           width="100%"
           scrolling="no"
         />
@@ -36,13 +36,13 @@ export default () => {
       <p id="callback">
         <MessageData data={messageData} />
       </p>
-      <div style={{ margin: '8px 0', fontSize: '13px' }}>
-        For details on how this works, see{' '}
+      <div style={{ margin: "8px 0", fontSize: "13px" }}>
+        For details on how this works, see{" "}
         <a href="http://davidjbradshaw.github.io/iframe-resizer/">
           http://davidjbradshaw.github.io/iframe-resizer/
         </a>
         .
       </div>
     </div>
-  )
-}
+  );
+};
